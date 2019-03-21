dcd0b8f60c7e0df29dc4a924994a387e75da2c2a
shaaran Lakshminarayanan <34472894+devshaaran@users.noreply.github.com>
2019-03-22 00:25:36 +0530

    Error gmft fix
    Gmft fix

--- server.go
+++ server.go
@@ -2,10 +2,11 @@ package main
 import "fmt"
 import "net/http"
 
-func HelloWorld(w http.ResposeWriter, r *http.Request)
-{	fmt.Fprintf(w,"HelloWorld")}
-func main()
-{
+func HelloWorld(w http.ResposeWriter, r *http.Request){
+	fmt.Fprintf(w,"HelloWorld")
+}
+func main(){
+
 	http.HandleFunc("/",helloWorld)
 	http.ListenandServe(":8080",nil)
 }
