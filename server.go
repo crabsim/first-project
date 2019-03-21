8f1b30acd844412383e356b5946cc90058c8309e
shaaran Lakshminarayanan <34472894+devshaaran@users.noreply.github.com>
2019-03-22 00:45:51 +0530

    Maj updt
    Updt

--- server.go
+++ server.go
@@ -2,11 +2,11 @@ package main
 import "fmt"
 import "net/http"
 
-func HelloWorld(w http.ResposeWriter, r *http.Request){
+func HelloWorld(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w,"HelloWorld")
 }
 func main(){
 
-	http.HandleFunc("/",helloWorld)
-	http.ListenandServe(":8080",nil)
+	http.HandleFunc("/",HelloWorld)
+	http.ListenAndServe(":8080",nil)
 }
